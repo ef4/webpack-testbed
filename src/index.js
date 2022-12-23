aeff83acf2ca584473f00b1ca051853a06a6033b
Edward Faulkner <edward@eaf4.com>
2022-12-23 17:43:38 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import { message } from "./target-1";
+import { thing } from "#made-up-package";
 console.log(message);
+console.log(thing);
--- webpack.config.js
+++ webpack.config.js
@@ -2,61 +2,83 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 class ExperimentalPlugin {
   apply(compiler) {
-    // let resolverPlugin = new ResolverPlugin();
-    // if (!compiler.options.resolve.plugins) {
-    //   compiler.options.resolve.plugins = [];
-    // }
-    // compiler.options.resolve.plugins.push(resolverPlugin);
-    // compiler.hooks.afterResolvers.tap("my-experiment", () => {
-    //   console.log(compiler);
-    //   debugger;
-    // });
+    let resolverPlugin = new ResolverPlugin();
+    if (!compiler.options.resolve.plugins) {
+      compiler.options.resolve.plugins = [];
+    }
+    compiler.options.resolve.plugins.push(resolverPlugin);
   }
 }
 
 // enhanced-resolve leaves its default hooks at stage 0. The important thing
-// here is that we want to later than those.
+// here is that we want to put some things earlier and some later than those.
+const beforeDefault = -10;
 const afterDefaults = 10;
 
 class ResolverPlugin {
   apply(resolver) {
-    // resolver
-    //   .getHook("raw-resolve")
-    //   .tapAsync("my-resolver-plugin", async (request, context, cb) => {
-    //     console.log(`alias ${request.request}`);
-    //     let target = resolver.ensureHook("internal-resolve");
-    //     cb();
-    //   });
-
+    // raw-resolve -> internal-resolve is the same place in the pipeline that
+    // webpack's built-in `resolve.alias` takes effect. It's supposed to take
+    // precedence over other resolving decisions.
     resolver
-      .getHook("described-resolve")
-      .tapAsync(
-        { name: "my-resolver-plugin", stage: afterDefaults },
-        async (request, context, callback) => {
-          console.log(`fallback ${request.request}`);
-          let target = resolver.ensureHook("internal-resolve");
-          if (request.request.endsWith("1")) {
-            let newRequest = {
-              ...request,
-              request: request.request.replace(/1$/, "2"),
-              fullSpecifier: false,
-            };
-            resolver.doResolve(
-              target,
-              newRequest,
-              "my experiment",
-              context,
-              (err, result) => {
-                if (err) return callback(err);
-                if (result) return callback(null, result);
-                return callback();
-              }
-            );
-          } else {
-            callback();
-          }
+      .getHook("raw-resolve")
+      .tapAsync("my-resolver-plugin", async (request, context, callback) => {
+        let target = resolver.ensureHook("internal-resolve");
+        if (request.request === "#made-up-package") {
+          let newRequest = {
+            ...request,
+            request: "./made-up-package.js",
+            fullySpecified: false,
+          };
+          resolver.doResolve(
+            target,
+            newRequest,
+            "my experiment",
+            context,
+            (err, result) => {
+              if (err) return callback(err);
+              if (result) return callback(null, result);
+              return callback();
+            }
+          );
+        } else {
+          callback();
         }
-      );
+      });
+
+    // described-resolve -> internal-resolve is the same place in the pipeline
+    // that webpack's built-in `resolve.fallback` takes effect. It's supposed to
+    // only run when the rest of resolving fails to find something.
+    resolver.getHook("described-resolve").tapAsync(
+      // we need to set the stage here because otherwise we end up before the
+      // built-in NextPlugin. Instead we want to behave like the built-in
+      // AliasPlugin that implements resolve.fallback -- it comes after
+      // NextPlugin.
+      { name: "my-resolver-plugin", stage: afterDefaults },
+      async (request, context, callback) => {
+        let target = resolver.ensureHook("internal-resolve");
+        if (request.request.endsWith("1")) {
+          let newRequest = {
+            ...request,
+            request: request.request.replace(/1$/, "2"),
+            fullySpecified: false,
+          };
+          resolver.doResolve(
+            target,
+            newRequest,
+            "my experiment",
+            context,
+            (err, result) => {
+              if (err) return callback(err);
+              if (result) return callback(null, result);
+              return callback();
+            }
+          );
+        } else {
+          callback();
+        }
+      }
+    );
   }
 }
 
@@ -66,8 +88,5 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin(), new ExperimentalPlugin()],
   resolve: {
     plugins: [new ResolverPlugin()],
-    // fallback: {
-    //   "./target-1": "./target-2",
-    // },
   },
 };
