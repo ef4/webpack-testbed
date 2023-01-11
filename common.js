73efa75ed638265ee65a1537d9db1ce5489aae17
Edward Faulkner <edward@eaf4.com>
2023-01-11 14:32:05 -0500

    establishing common resolver API

--- common.js
+++ common.js
@@ -1,5 +1,57 @@
-exports.fallback = function fallback(original) {
-  if (original.endsWith("1")) {
-    return original.replace(/1$/, "2");
+const path = require("path");
+
+/*
+  Establishing common API for customize resolving.
+
+  Both hooks have the same signature and can return
+   - undefined to not handle a request at all
+   - `{ alias }` to redirect resolving toward a different importPath and/or fromFile
+   - `{ virtual }` to provide a module on the fly
+*/
+
+exports.Resolver = class Resolver {
+  beforeResolve(original, fromFile) {
+    // demonstrate priority aliasing
+    if (original === "#made-up-package") {
+      return {
+        alias: {
+          importPath: "./made-up-package.js",
+          fromFile,
+        },
+      };
+    }
+
+    // demonstrate emitting virtual modules (which will be our solution for
+    // externals)
+    if (original === "my-virtual-package") {
+      return {
+        virtual: {
+          filename: "virtual-stuff/index.js",
+          content: 'export const stuff = "this is the stuff"',
+        },
+      };
+    }
+
+    // demonstrate resolving from an alternate location
+    if (original === "co" && fromFile === path.resolve(__dirname, "src")) {
+      return {
+        alias: {
+          importPath: original,
+          fromFile: path.resolve(__dirname, "../embroider/package.json"),
+        },
+      };
+    }
+  }
+
+  fallbackResolve(original, fromFile) {
+    // demonstrate fallback aliasing
+    if (original.endsWith("1")) {
+      return {
+        alias: {
+          importPath: original.replace(/1$/, "2"),
+          fromFile,
+        },
+      };
+    }
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const VirtualModulesPlugin = require("webpack-virtual-modules");
-const path = require("path");
-const { fallback } = require("./common");
+const { Resolver } = require("./common");
 
 class ExperimentalPlugin {
   apply(compiler) {
@@ -25,27 +24,49 @@ class ExperimentalPlugin {
 class ResolverPlugin {
   constructor(vfs) {
     this.vfs = vfs;
+    this.resolver = new Resolver();
   }
 
-  _alias(original) {
-    // demonstrate priority aliasing
-    if (original === "#made-up-package") {
-      return "./made-up-package.js";
+  #resolve(result, resolver, request, context, callback) {
+    if (!result) {
+      callback();
+      return;
     }
 
-    // and emitting virtual modules (which will be our solution for externals)
-    if (original === "my-virtual-package") {
+    if ("virtual" in result) {
       this.vfs.writeModule(
-        "node_modules/my-virtual-package/index.js",
-        'export const stuff = "this is the stuff"'
+        `node_modules/${result.virtual.filename}`,
+        result.virtual.content
       );
+      result = {
+        alias: {
+          importPath: result.virtual.filename,
+          fromFile: request.path,
+        },
+      };
     }
-  }
 
-  // demonstrate resolving *from* a different package than the real one
-  _rehome(original, fromPath) {
-    if (original === "co" && fromPath === path.resolve(__dirname, "src")) {
-      return path.resolve(__dirname, "../embroider");
+    if ("alias" in result) {
+      let newRequest = {
+        ...request,
+        request: result.alias.importPath,
+        path: result.alias.fromFile,
+        fullySpecified: false,
+      };
+      resolver.doResolve(
+        resolver.ensureHook("internal-resolve"),
+        newRequest,
+        "my experiment",
+        context,
+        (err, result) => {
+          if (err) return callback(err);
+          if (result) return callback(null, result);
+          return callback();
+        }
+      );
+      return;
+    } else {
+      throw new Error(`bug: unexpected result ${result}`);
     }
   }
 
@@ -56,49 +77,8 @@ class ResolverPlugin {
     resolver
       .getHook("raw-resolve")
       .tapAsync("my-resolver-plugin", async (request, context, callback) => {
-        let target = resolver.ensureHook("internal-resolve");
-        let aliased = this._alias(request.request);
-        if (aliased) {
-          let newRequest = {
-            ...request,
-            request: aliased,
-            fullySpecified: false,
-          };
-          resolver.doResolve(
-            target,
-            newRequest,
-            "my experiment",
-            context,
-            (err, result) => {
-              if (err) return callback(err);
-              if (result) return callback(null, result);
-              return callback();
-            }
-          );
-          return;
-        }
-
-        let rehomed = this._rehome(request.request, request.path);
-        if (rehomed) {
-          let newRequest = {
-            ...request,
-            path: rehomed,
-          };
-          resolver.doResolve(
-            target,
-            newRequest,
-            "my experiment",
-            context,
-            (err, result) => {
-              if (err) return callback(err);
-              if (result) return callback(null, result);
-              return callback();
-            }
-          );
-          return;
-        }
-
-        callback();
+        let result = this.resolver.beforeResolve(request.request, request.path);
+        this.#resolve(result, resolver, request, context, callback);
       });
 
     // described-resolve -> internal-resolve is the same place in the pipeline
@@ -114,28 +94,11 @@ class ResolverPlugin {
       // defaults (tapable assigned them stage 0 by default).
       { name: "my-resolver-plugin", stage: 10 },
       async (request, context, callback) => {
-        let aliased = fallback(request.request);
-        if (aliased) {
-          let target = resolver.ensureHook("internal-resolve");
-          let newRequest = {
-            ...request,
-            request: aliased,
-            fullySpecified: false,
-          };
-          resolver.doResolve(
-            target,
-            newRequest,
-            "my experiment",
-            context,
-            (err, result) => {
-              if (err) return callback(err);
-              if (result) return callback(null, result);
-              return callback();
-            }
-          );
-        } else {
-          callback();
-        }
+        let result = this.resolver.fallbackResolve(
+          request.request,
+          request.path
+        );
+        this.#resolve(result, resolver, request, context, callback);
       }
     );
   }
